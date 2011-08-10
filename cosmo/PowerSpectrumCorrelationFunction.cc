4e499ec37611cd90028f8fe8eed815b9ba993b4c
David Kirkby <dkirkby@uci.edu>
2011-08-09 17:43:45 -0700

    add correlation fn calc method

--- cosmo/PowerSpectrumCorrelationFunction.cc
+++ cosmo/PowerSpectrumCorrelationFunction.cc
@@ -1,9 +1,26 @@
 // Created 09-Aug-2011 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
 
 #include "cosmo/PowerSpectrumCorrelationFunction.h"
+#include "cosmo/RuntimeError.h"
 
 namespace local = cosmo;
 
-local::PowerSpectrumCorrelationFunction::PowerSpectrumCorrelationFunction() { }
+local::PowerSpectrumCorrelationFunction::PowerSpectrumCorrelationFunction(
+PowerSpectrumPtr powerSpectrum, double rmin, double rmax, int nr)
+: _powerSpectrum(powerSpectrum), _rmin(rmin), _rmax(rmax), _nr(nr)
+{
+    if(rmin < 0) {
+        throw RuntimeError("PowerSpectrumCorrelationFunction: invalid rmin < 0.");
+    }
+    if(rmax <= rmin) {
+        throw RuntimeError("PowerSpectrumCorrelationFunction: invalid rmax <= rmin.");
+    }
+    if(nr < 2) {
+        throw RuntimeError("PowerSpectrumCorrelationFunction: invalid nr < 2.");
+    }
+}
 
 local::PowerSpectrumCorrelationFunction::~PowerSpectrumCorrelationFunction() { }
+
+double local::PowerSpectrumCorrelationFunction::operator()(double rMpch) const {
+}
--- cosmo/PowerSpectrumCorrelationFunction.h
+++ cosmo/PowerSpectrumCorrelationFunction.h
@@ -3,12 +3,24 @@
 #ifndef COSMO_POWER_SPECTRUM_CORRELATION_FUNCTION
 #define COSMO_POWER_SPECTRUM_CORRELATION_FUNCTION
 
+#include "cosmo/types.h"
+
 namespace cosmo {
+    // Represents the isotropic correlation function of an isotropic 3D power spectrum.
 	class PowerSpectrumCorrelationFunction {
 	public:
-		PowerSpectrumCorrelationFunction();
+	    // Creates a correlation function from the power spectrum provided that is valid
+	    // from rmin-rmax (in Mpc/h) and which uses nr equally-spaced interpolation points
+	    // over this range.
+		PowerSpectrumCorrelationFunction(PowerSpectrumPtr powerSpectrum,
+		    double rmin, double rmax, int nr = 8192);
 		virtual ~PowerSpectrumCorrelationFunction();
+		// Returns the correlation function evaluated at the specified radius in Mpc/h.
+        double operator()(double rMpch) const;
 	private:
+        PowerSpectrumPtr _powerSpectrum;
+        double _rmin, _rmax;
+        int _nr;
 	}; // PowerSpectrumCorrelationFunction
 } // cosmo
 
