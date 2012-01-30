d7f4989eb9a27264de9c7208a59846c55123912e
David Kirkby <dkirkby@uci.edu>
2012-01-29 18:07:53 -0800

    Rework BAO fitting parameterization.

--- src/cosmocalc.cc
+++ src/cosmocalc.cc
@@ -22,17 +22,17 @@ namespace po = boost::program_options;
 
 class BaoFitPower {
 public:
-    BaoFitPower(double scale, double alpha, double sigma,
+    BaoFitPower(double amplitude, double scale, double sigma,
         cosmo::PowerSpectrumPtr full, cosmo::PowerSpectrumPtr nowiggles)
-    : _scale(scale), _alpha(alpha), _sigsq(sigma*sigma), _full(full), _nowiggles(nowiggles)
+    : _amplitude(amplitude), _scale(scale), _sigsq(sigma*sigma), _full(full), _nowiggles(nowiggles)
     { }
     double operator()(double k) const {
-        double ak(_alpha*k), smooth(std::exp(-ak*ak*_sigsq/2));
+        double ak(k/_scale), smooth(std::exp(-ak*ak*_sigsq/2));
         double fullPower = (*_full)(ak), nowigglesPower = (*_nowiggles)(ak);
-        return _scale*smooth*(fullPower - nowigglesPower) + nowigglesPower;
+        return _amplitude*smooth*(fullPower - nowigglesPower) + nowigglesPower;
     }
 private:
-    double _scale, _alpha, _sigsq;
+    double _amplitude, _scale, _sigsq;
     cosmo::PowerSpectrumPtr _full, _nowiggles;
 }; // BaoFitPower
 
