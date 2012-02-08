60a30b0a16c2dc8bff2bf25b02fdcede1fe61f8b
David Kirkby <dkirkby@uci.edu>
2012-02-08 06:32:15 -0800

    Overlay oversampled model as contours.

--- script/plotBaoFit.cc
+++ script/plotBaoFit.cc
@@ -9,22 +9,29 @@ void plotBaoFit(const char *filename = "fit.dat") {
     gROOT->SetStyle("Plain");
     gStyle->SetOptStat(0);
     gStyle->SetOptTitle(0);
-    gStyle->SetPalette(1,0);
+    
+    Double_t r[3] = { 0.0, 1.0, 1.0 };
+    Double_t g[3] = { 0.0, 1.0, 0.0 };
+    Double_t b[3] = { 1.0, 1.0, 0.0 };
+    Double_t s[3] = { 0.0, 0.5, 1.0 };
+    TColor::CreateGradientColorTable(3,s,r,g,b,256);
+    //gStyle->SetPalette(1,0);
     
     std::ifstream in(filename);
     // Read the binning parameters.
-    int nll,nsep,nz;
+    int nll,nsep,nz,ndata,oversampling;
     double minll,minsep,minz,dll,dsep,dz;
     in >> nll >> minll >> dll;
     in >> nsep >> minsep >> dsep;
     in >> nz >> minz >> dz;
+    in >> ndata >> oversampling;
     
     // Initialize the drawing canvas.
     canvas = new TCanvas("canvas","canvas",nz*400,800);
     canvas->Divide(nz,2);
 
     // Create 2D histograms in (ll,sep) for each redshift.
-    for(iz = 0; iz < nz; ++iz) {
+    for(int iz = 0; iz < nz; ++iz) {
         double z(minz + (iz+0.5)*dz);
         TH2F *dataHist = (TH2F *)gDirectory->Get(Form("data%d",iz));
         if(dataHist) continue;
@@ -33,19 +40,15 @@ void plotBaoFit(const char *filename = "fit.dat") {
         dataHist->SetXTitle("Pair angular separation (arcmin)");
         dataHist->SetYTitle("Pair LOS separation log(lam2/lam1)");
         TH2F *pullHist = dataHist->Clone(Form("pull%d",iz));
+        TH2F *modelHist = new TH2F(Form("model%d",iz),Form("Model predictions for z = %.2f",z),
+            oversampling*nsep,minsep,minsep+nsep*dsep,oversampling*nll,minll,minll+nll*dll);
     }
     
     // Loop over bin data in the input file.
-    while(in.good() && !in.eof()) {
-        int index;
-        double data,pull;
+    int index;
+    double data,pull;
+    for(int k = 0; k < ndata; ++k) {
         in >> index >> data >> pull;
-        std::cout << index << ' ' << data << ' ' << pull << std::endl;
-        if(in.eof()) break;
-        if(!in.good()) {
-            std::cout << "Error reading input file." << std::endl;
-            return;
-        }
         int iz = index % nz;
         TH2F *dataHist = (TH2F *)gDirectory->Get(Form("data%d",iz));
         TH2F *pullHist = (TH2F *)gDirectory->Get(Form("pull%d",iz));
@@ -54,16 +57,35 @@ void plotBaoFit(const char *filename = "fit.dat") {
         dataHist->SetBinContent(isep,ill,data);
         pullHist->SetBinContent(isep,ill,pull);
     }
+    
+    // Loop over model predictions in the input file.
+    double pred;
+    for(int iz = 0; iz < nz; ++iz) {
+        TH2F *modelHist = (TH2F *)gDirectory->Get(Form("model%d",iz));
+        for(int isep = 0; isep < oversampling*nsep; ++isep) {
+            for(int ill = 0; ill < oversampling*nll; ++ill) {
+                in >> pred;
+                modelHist->SetBinContent(isep+1,ill+1,pred);
+            }
+        }
+    }
 
     // Draw plots.
+    double zmax=1e-4;
     for(iz = 0; iz < nz; ++iz) {
         TH2F *dataHist = (TH2F *)gDirectory->Get(Form("data%d",iz));
+        TH2F *modelHist = (TH2F *)gDirectory->Get(Form("model%d",iz));
         canvas->cd(iz+1);
-        canvas->GetPad(iz+1)->SetLogz();
+        //canvas->GetPad(iz+1)->SetLogz();
+        dataHist->SetMaximum(+zmax);
+        dataHist->SetMinimum(-zmax);
         dataHist->Draw("col");
+        modelHist->SetMaximum(+zmax);
+        modelHist->SetMinimum(-zmax);
+        modelHist->Draw("cont3same");
         TH2F *pullHist = (TH2F *)gDirectory->Get(Form("pull%d",iz));
-        pullHist->SetMinimum(-2);
-        pullHist->SetMaximum(+2);
+        pullHist->SetMinimum(-3);
+        pullHist->SetMaximum(+3);
         canvas->cd(nz+iz+1);
         pullHist->Draw("col");                
     }    
