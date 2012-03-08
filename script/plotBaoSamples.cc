8cf9869c0f56ab0ade3af39cb91ccbae6ff7bc4a
David Kirkby <dkirkby@uci.edu>
2012-03-08 08:40:00 -0800

    Generalize script to work with any pair of fit parameters.

--- script/plotBaoSamples.cc
+++ script/plotBaoSamples.cc
@@ -6,33 +6,41 @@ TCanvas *canvas, *canvas2, *canvas3;
 int count = 0;
 int colors[15] = { 1,2,3,4,5,6,7,8,9,46,28,30,33,38,41 };
 
+char name[9][16] = { "alpha", "bias", "beta", "amp", "scale", "xio", "a0", "a1", "a2" };
+
+int bins[9] =  {     50,    50,  50,  50,   50,     50,  50,  50,    50  };
+double xlo[9]   = { -0.5, -0.05, -1., -1., 0.65, -1e-3, -3., -10., -10. };
+double xhi[9]   = {  9.5,  0.50, 10.,  5., 1.35,  1e-3,  3.,  10.,  10. };
+
 TH1F *scaleAll, *ampAll;
 
-void analyze(int index, const char *pattern) {
+void analyze(int index, const char *pattern, int p1, int p2) {
     
     int color = colors[count];
-
+    
     // Load the tree
     TFile *file = new TFile(Form(pattern,index),"READ");
     TTree *tree = (TTree*)file->Get("bs");
 
     // Plot a scatter plot of each bootstrap sample in (scale,amp)
     canvas->cd(1);
-    TH2F *scaleAmpHist = new TH2F(Form("scaleamp%d",count),";BAO Scale;BAO Amplitude",
-        100,0.65,1.35,100,-1.,5.);
+    TH2F *scaleAmpHist = new TH2F(Form("%s%s%d",name[p1],name[p2],count),Form(";%s;%s",name[p1],name[p2]),
+        100,xlo[p1],xhi[p1],100,xlo[p2],xhi[p2]);
     scaleAmpHist->SetMarkerColor(color);
     scaleAmpHist->SetMarkerStyle(4);
     scaleAmpHist->SetMarkerSize(0.4);
-    tree->Draw(Form("amp:scale>>scaleamp%d",count),"","goff");
+    tree->Draw(Form("%s:%s>>%s%s%d",name[p2],name[p1],name[p1],name[p2],count),"","goff");
     scaleAmpHist->Draw((count ? "same":""));
     
     // Plot a histogram of bootstrap scale values (not plotted)
-    TH1F *scaleHist = new TH1F(Form("scale%d",count),";BAO Scale;Bootstrap Trials",50,0.65,1.35);
-    tree->Draw(Form("scale>>scale%d",count),"","goff");
+    TH1F *scaleHist = new TH1F(Form("%s%d",name[p1],count),Form(";%s;Bootstrap Trials",name[p1]),
+        bins[p1],xlo[p1],xhi[p1]);
+    tree->Draw(Form("%s>>%s%d",name[p1],name[p1],count),"","goff");
     
     // Plot a histogram of bootstrap amplitude values (not plotted)
-    TH1F *ampHist = new TH1F(Form("amp%d",count),";BAO Amplitude;Bootstrap Trials",50,-1,5);
-    tree->Draw(Form("amp>>amp%d",count),"","goff");
+    TH1F *ampHist = new TH1F(Form("%s%d",name[p2],count),Form(";%s;Bootstrap Trials",name[p2]),
+        bins[p2],xlo[p2],xhi[p2]);
+    tree->Draw(Form("%s>>%s%d",name[p2],name[p2],count),"","goff");
     
     // Plot per-realization bootstrap distributions of scale and amplitude.
     canvas2->cd(count+1);
@@ -68,23 +76,34 @@ void analyze(int index, const char *pattern) {
     count++;
 }
 
-void plotBaoSamples() {
+/**
+0   ||     Alpha
+1   ||      Bias
+2   ||      Beta
+3   ||  BAO Ampl
+4   || BAO Scale
+5   ||    BB xio
+6   ||     BB a0
+7   ||     BB a1
+8   ||     BB a2
+**/
+void plotBaoSamples(int p1 = 4, int p2 = 3) {
     // Initialize graphics options.
     gROOT->SetStyle("Plain");
     gStyle->SetOptStat(0);
     gStyle->SetOptTitle(0);
     
-    // Initialize the drawing canvas.
+    // Initialize the drawing canvases.
     canvas = new TCanvas("canvas","canvas",1000,1000);
     canvas->Divide(2,2);
-    
     canvas2 = new TCanvas("canvas2","canvas2",1500,900);
     canvas2->Divide(5,3,0,0);
     canvas3 = new TCanvas("canvas3","canvas3",1500,900);
     canvas3->Divide(5,3,0,0);
     
     canvas->cd(4);
-    TH2F *frame4 = new TH2F("frame4",";BAO Scale;BAO Amplitude",1,0.65,1.35,1,-1.,5.);
+    TH2F *frame4 = new TH2F("frame4",Form(";%s;%s",name[p1],name[p2]),
+        1,xlo[p1],xhi[p1],1,xlo[p2],xhi[p2]);
     frame4->Draw();
     
     count = 0;    
@@ -92,7 +111,7 @@ void plotBaoSamples() {
     const char *with_noise = "results/delta_diag_n_%d.root";
 
     for(int index = 1; index <= 15; ++index) {
-        analyze(index, no_noise);
+        analyze(index, with_noise, p1, p2);
     }
     
     canvas->cd(2);
