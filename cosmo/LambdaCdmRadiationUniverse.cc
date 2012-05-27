a1e194fb87fbbaac8fd821219a16074ee322cf0a
David Kirkby <dkirkby@uci.edu>
2012-05-26 17:39:36 -0700

    Add skeleton homogeneous universe with radiation added.

---
+++ cosmo/LambdaCdmRadiationUniverse.cc
@@ -0,0 +1,9 @@
+// Created 26-May-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#include "cosmo/LambdaCdmRadiationUniverse.h"
+
+namespace local = cosmo;
+
+local::LambdaCdmRadiationUniverse::LambdaCdmRadiationUniverse() { }
+
+local::LambdaCdmRadiationUniverse::~LambdaCdmRadiationUniverse() { }
---
+++ cosmo/LambdaCdmRadiationUniverse.h
@@ -0,0 +1,15 @@
+// Created 26-May-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#ifndef COSMO_LAMBDA_CDM_RADIATION_UNIVERSE
+#define COSMO_LAMBDA_CDM_RADIATION_UNIVERSE
+
+namespace cosmo {
+	class LambdaCdmRadiationUniverse {
+	public:
+		LambdaCdmRadiationUniverse();
+		virtual ~LambdaCdmRadiationUniverse();
+	private:
+	}; // LambdaCdmRadiationUniverse
+} // cosmo
+
+#endif // COSMO_LAMBDA_CDM_RADIATION_UNIVERSE
