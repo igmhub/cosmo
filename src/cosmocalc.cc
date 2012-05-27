25006645b089c66081163fab9f66f7032dcb49a3
David Kirkby <dkirkby@uci.edu>
2012-05-26 18:34:11 -0700

    Use new radiation model in cosmocalc.

--- src/cosmocalc.cc
+++ src/cosmocalc.cc
@@ -27,7 +27,7 @@ int main(int argc, char **argv) {
     
     // Configure command-line option processing
     po::options_description cli("Cosmology calculator");
-    double OmegaLambda,OmegaMatter,OmegaBaryon,hubbleConstant,cmbTemp,spectralIndex,sigma8,
+    double OmegaMatter,OmegaBaryon,hubbleConstant,cmbTemp,spectralIndex,sigma8,
         zval,kval,kmin,kmax,r1d,rmin,rmax,baoAmplitude,baoSigma,baoScale;
     double bbandP,bbandCoef,bbandKmin,bbandRmin,bbandR0,bbandVar;
     int nk,nr;
@@ -35,10 +35,8 @@ int main(int argc, char **argv) {
     cli.add_options()
         ("help,h", "Prints this info and exits.")
         ("verbose", "Prints additional information.")
-        ("omega-lambda", po::value<double>(&OmegaLambda)->default_value(0.728),
-            "Present-day value of OmegaLambda.")
-        ("omega-matter", po::value<double>(&OmegaMatter)->default_value(0),
-            "Present-day value of OmegaMatter or zero for 1-OmegaLambda.")
+        ("omega-matter", po::value<double>(&OmegaMatter)->default_value(0.272),
+            "Present-day value of OmegaMatter.")
         ("omega-baryon", po::value<double>(&OmegaBaryon)->default_value(0.0456),
             "Present-day value of OmegaBaryon, must be <= OmegaMatter.")
         ("hubble-constant", po::value<double>(&hubbleConstant)->default_value(0.704),
@@ -140,9 +138,8 @@ int main(int argc, char **argv) {
     if(periodicWiggles) baoOption = cosmo::BaryonPerturbations::PeriodicOscillation;
 
     // Build the homogeneous cosmology we will use.
-    if(OmegaMatter == 0) OmegaMatter = 1 - OmegaLambda;
     cosmo::AbsHomogeneousUniversePtr cosmology(
-        new cosmo::LambdaCdmUniverse(OmegaLambda,OmegaMatter));
+        new cosmo::LambdaCdmRadiationUniverse(OmegaMatter,0,hubbleConstant,cmbTemp));
     double growthFactor = cosmology->getGrowthFunction(zval)/cosmology->getGrowthFunction(0);
     if(verbose) {
         // Print homogeneous cosmology info.
