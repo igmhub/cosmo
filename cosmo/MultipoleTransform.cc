5a0e5045d2b128686ec05cffa2eba7d70cf8f8fa
David Kirkby <dkirkby@uci.edu>
2014-01-10 14:10:00 -0800

    Add skeleton new class MultipoleTransform

---
+++ cosmo/MultipoleTransform.cc
@@ -0,0 +1,12 @@
+// Created 10-Jan-2014 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#include "cosmo/MultipoleTransform.h"
+
+namespace local = cosmo;
+
+local::MultipoleTransform::MultipoleTransform(likely::GenericFunctionPtr func,
+Type type, int ell, double umin, double umax, double eps)
+{
+}
+
+local::MultipoleTransform::~MultipoleTransform() { }
---
+++ cosmo/MultipoleTransform.h
@@ -0,0 +1,28 @@
+// Created 10-Jan-2014 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#ifndef COSMO_MULTIPOLE_TRANSFORM
+#define COSMO_MULTIPOLE_TRANSFORM
+
+#include "likely/function.h"
+
+namespace cosmo {
+	class MultipoleTransform {
+	// Calculates 2D (Hankel) and 3D (spherical Bessel) multipole transforms of
+	// an arbitrary real-valued function.
+	public:
+		enum Type { SphericalBessel, Hankel };
+		// Creates a new transform object for the specified func(u) that evaluates:
+		//
+		//   T(v) = Integrate[ S(ell,u,v)*func(u) , {u,0,Infinity} ]
+		//
+		// where S = u^2 j_ell(u*v) when type is SphericalBessel or
+		// S = u J_ell(u*v) when type is Hankel. The result will be value to
+		// the accuracy eps over the range umin < u < umax.
+		MultipoleTransform(likely::GenericFunctionPtr func, Type type, int ell,
+			double umin, double umax, double eps);
+		virtual ~MultipoleTransform();
+	private:
+	}; // MultipoleTransform
+} // cosmo
+
+#endif // COSMO_MULTIPOLE_TRANSFORM
