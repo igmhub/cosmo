a57d3ad2b42f037c93b2da4b2a45c6f01df9bd41
David Kirkby <dkirkby@uci.edu>
2014-01-19 10:07:02 -0800

    Implement AdaptiveMultipoleMoment ctor

--- cosmo/AdaptiveMultipoleTransform.cc
+++ cosmo/AdaptiveMultipoleTransform.cc
@@ -1,9 +1,38 @@
 // Created 19-Jan-2014 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
 
 #include "cosmo/AdaptiveMultipoleTransform.h"
+#include "cosmo/RuntimeError.h"
 
 namespace local = cosmo;
 
-local::AdaptiveMultipoleTransform::AdaptiveMultipoleTransform() { }
+local::AdaptiveMultipoleTransform::AdaptiveMultipoleTransform(MultipoleTransform::Type type,
+int ell, std::vector<double>const &vpoints, double relerr, double abserr, double abspow)
+: _type(type), _ell(ell), _vpoints(vpoints), _relerr(relerr), _abserr(abserr), _abspow(abspow)
+{
+	// Input parameter validation
+	if(_type != MultipoleTransform::SphericalBessel && _type != MultipoleTransform::Hankel) {
+		throw RuntimeError("AdaptiveMultipoleTransform: invalid type.");
+	}
+	if(_ell < 0) {
+		throw RuntimeError("AdaptiveMultipoleTransform: expected ell >= 0.");
+	}
+	if(_relerr <= 0 && _abserr <= 0) {
+		throw RuntimeError("AdaptiveMultipoleTransform: invalid termination criteria.");
+	}
+	int nv(_vpoints.size());
+	if(nv < 2) {
+		throw RuntimeError("AdaptiveMultipoleTransform: expected at least 2 vpoints.");
+	}
+	for(int iv = 1; iv < nv; ++iv) {
+		if(_vpoints[iv] <= _vpoints[iv-1]) {
+			throw RuntimeError("AdaptiveMultipoleTransform: vpoints not increasing.");
+		}
+	}
+	_vmin = _vpoints.front();
+	_vmax = _vpoints.back();
+	if(_vmin <= 0) {
+		throw RuntimeError("AdaptiveMultipoleTransform: expected vmin > 0.");
+	}
+}
 
 local::AdaptiveMultipoleTransform::~AdaptiveMultipoleTransform() { }
--- cosmo/AdaptiveMultipoleTransform.h
+++ cosmo/AdaptiveMultipoleTransform.h
@@ -3,12 +3,30 @@
 #ifndef COSMO_ADAPTIVE_MULTIPOLE_TRANSFORM
 #define COSMO_ADAPTIVE_MULTIPOLE_TRANSFORM
 
+#include "MultipoleTransform.h"
+
+#include <vector>
+
 namespace cosmo {
 	class AdaptiveMultipoleTransform {
+	// Uses the MultipoleTransform class to calculate transforms but replaces the
+	// veps numerical control parameter with accuracy criteria that are used to
+	// automatically set veps. Also takes a function pointer as input, instead of
+	// requiring the user to tabulate function values.
 	public:
-		AdaptiveMultipoleTransform();
+		// Creates a new transformer of the specified type and multipole. Subsequent
+		// transforms will be provided at the specified vpoints, which will also be
+		// used to adaptively monitor numerical errors. The numerical termination
+		// criteria is that |f(2*veps) - f(veps)| < max(abserr*v^abspow,relerr*|f(veps)|)
+		// for each point v in vpoints.
+		AdaptiveMultipoleTransform(MultipoleTransform::Type type, int ell,
+			std::vector<double> const &vpoints, double relerr, double abserr, double abspow = 0);
 		virtual ~AdaptiveMultipoleTransform();
 	private:
+		MultipoleTransform::Type _type;
+		int _ell;
+		std::vector<double> _vpoints;
+		double _relerr, _abserr, _abspow, _vmin, _vmax;
 	}; // AdaptiveMultipoleTransform
 } // cosmo
 
