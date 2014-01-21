a74f93206895ca52e218c31510589860246a0907
David Kirkby <dkirkby@uci.edu>
2014-01-21 14:01:54 -0800

    More work on ctor

--- cosmo/DistortedPowerCorrelation.cc
+++ cosmo/DistortedPowerCorrelation.cc
@@ -1,24 +1,51 @@
 // Created 20-Jan-2014 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
 
 #include "cosmo/DistortedPowerCorrelation.h"
+#include "cosmo/AdaptiveMultipoleTransform.h"
 
 #include "cosmo/RuntimeError.h"
 
 namespace local = cosmo;
 
 local::DistortedPowerCorrelation::DistortedPowerCorrelation(
-TabulatedPowerCPtr power, RMuFunctionCPtr distortion, int ellMax, double rmin, double rmax)
-: _power(power), _distortion(distortion), _ellMax(ellMax), _rmin(rmin), _rmax(rmax)
+TabulatedPowerCPtr power, RMuFunctionCPtr distortion, double rmin, double rmax, int nr,
+int ellMax, bool symmetric, double relerr, double abserr, double abspow)
+: _power(power), _distortion(distortion), _ellMax(ellMax), _symmetric(symmetric),
+_relerr(relerr), _abserr(abserr), _abspow(abspow)
 {
-	if(ellMax < 0) {
-		throw RuntimeError("DistortedPowerCorrelation: expected ellMax >= 0.");
-	}
 	if(rmax <= rmin) {
 		throw RuntimeError("DistortedPowerCorrelation: expected rmin < rmax.");
 	}
 	if(rmin <= 0) {
 		throw RuntimeError("DistortedPowerCorrelation: expected rmin > 0.");
 	}
+	if(nr < 2) {
+		throw RuntimeError("DistortedPowerCorrelation: expected nr >= 2.");
+	}
+	if(ellMax < 0) {
+		throw RuntimeError("DistortedPowerCorrelation: expected ellMax >= 0.");
+	}
+	if(symmetric && (ellMax%2 == 1)) {
+		throw RuntimeError("DistortedPowerCorrelation: expected even ellMax when symmetric.");
+	}
+	// initialize the r grid we will use for interpolation
+	_rgrid.reserve(nr);
+	double dr = (rmax - rmin)/(nr-1.);
+	for(int i = 0; i < nr; ++i) {
+		_rgrid.push_back(rmin + dr*i);
+	}
+	// create a transform object for each moment
+	int dell = symmetric ? 2 : 1;
+	_moments.reserve(symmetric ? ellMax/2 : ellMax);
+	for(int ell = 0; ell < ellMax; ell += dell) {
+		AdaptiveMultipoleTransformPtr amt(new AdaptiveMultipoleTransform(
+			MultipoleTransform::SphericalBessel,ell,_rgrid,relerr,abserr,abspow));
+		_moments.push_back(amt);
+	}
 }
 
 local::DistortedPowerCorrelation::~DistortedPowerCorrelation() { }
+
+void local::DistortedPowerCorrelation::initialize() {
+
+}
--- cosmo/DistortedPowerCorrelation.h
+++ cosmo/DistortedPowerCorrelation.h
@@ -5,19 +5,39 @@
 
 #include "cosmo/types.h"
 
+#include "boost/smart_ptr.hpp"
+
+#include <vector>
+
 namespace cosmo {
+	class AdaptiveMultipoleTransform;
 	class DistortedPowerCorrelation {
 	// Represents the 3D correlation function corresponding to an isotropic power
 	// spectrum that is distorted by a multiplicative function of (r,mu).
 	public:
+		// Creates a new distorted power correlation function using the specified
+		// isotropic power P(k) and distortion function D(k,mu). The resulting
+		// correlation function will be valid over [rmin,rmax] and estimated
+		// using multipoles up to ellMax that are interpolated using nr equally-spaced
+		// points in r. If symmetric is true, then only even multipoles are used.
+		// The desired accuracy is specified by relerr, abserr, and abspow, such that
+		// the difference between the true and estimated xi(r,mu) satisfies:
+		// |true-est| < max(abserr*r^abspow,true*true)
 		DistortedPowerCorrelation(TabulatedPowerCPtr power, RMuFunctionCPtr distortion,
-			int ellMax, double rmin, double rmax);
+			double rmin, double rmax, int nr, int ellMax, bool symmetric = true,
+			double relerr = 1e-2, double abserr = 1e-3, double abspow = 0);
 		virtual ~DistortedPowerCorrelation();
+		// Initializes our multipole estimates and correlation transforms.
+		void initialize();
 	private:
 		TabulatedPowerCPtr _power;
 		RMuFunctionCPtr _distortion;
+		double _relerr,_abserr,_abspow;
 		int _ellMax;
-		double _rmin,_rmax;
+		bool _symmetric;
+		std::vector<double> _rgrid;
+		typedef boost::shared_ptr<AdaptiveMultipoleTransform> AdaptiveMultipoleTransformPtr;
+		std::vector<AdaptiveMultipoleTransformPtr> _moments;
 	}; // DistortedPowerCorrelation
 } // cosmo
 
