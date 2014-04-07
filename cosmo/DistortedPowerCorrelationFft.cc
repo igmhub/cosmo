d832cc00aaf651fb0f37da3996206ad7aa8ba44c
Michael Blomqvist <cblomqvi@uci.edu>
2014-04-07 14:23:33 -0700

    Correct the k space grid and the indexing for the interpolation vector

--- cosmo/DistortedPowerCorrelationFft.cc
+++ cosmo/DistortedPowerCorrelationFft.cc
@@ -31,34 +31,34 @@ RMuFunctionCPtr distortion, double spacing, int nx, int ny, int nz)
 : _power(power), _distortion(distortion), _spacing(spacing), _nx(nx), _ny(ny), _nz(nz), _pimpl(new Implementation())
 {	
 #ifdef HAVE_LIBFFTW3
-	// allocate data array
+	// Allocate data array.
 	_pimpl->data = (FFTW(complex)*) FFTW(malloc)(sizeof(FFTW(complex)) * nx*ny*nz);
 #else
     throw RuntimeError("DistortedPowerCorrelationFft: package not built with FFTW3.");
 #endif
 	double twopi(8*std::atan(1));
-    // initialize the k space grid that will be used for evaluating the power spectrum
+    // Initialize the k space grid that will be used for evaluating the power spectrum.
+    // Note that the grid is centered on (0,0,0) and grid points wrap around in a specific order.
     double dkx(twopi/(nx*spacing)), dky(twopi/(ny*spacing)), dkz(twopi/(nz*spacing));
-    double kmin = 0.000001;
 	_kxgrid.reserve(nx);
 	_kygrid.reserve(ny);
 	_kzgrid.reserve(nz);
 	for(int ix = 0; ix < nx; ++ix){
-        double kx = ix*dkx + kmin;
+        double kx = (ix > nx/2 ? ix-nx : ix)*dkx;
         _kxgrid.push_back(kx);
     }
     for(int iy = 0; iy < ny; ++iy){
-        double ky = iy*dky + kmin;
+        double ky = (iy > ny/2 ? iy-ny : iy)*dky;
         _kygrid.push_back(ky);
     }
     for(int iz = 0; iz < nz; ++iz){
-        double kz = iz*dkz + kmin;
+        double kz = (iz > nz/2 ? iz-nz : iz)*dkz;
         _kzgrid.push_back(kz);
     }
-    // calculate normalisation
+    // Calculate normalization.
     _norm = nx*ny*nz*spacing*spacing*spacing;
-    // initialize the array that will be used for bicubic interpolation
-    _xi.reset(new double[nx*ny]);
+    // Initialize the array that will be used for bicubic interpolation.
+    _xi.reset(new double[(nx/2+1)*(ny/2+1)]);
 }
 
 local::DistortedPowerCorrelationFft::~DistortedPowerCorrelationFft() {
@@ -88,34 +88,46 @@ double local::DistortedPowerCorrelationFft::getCorrelation(double r, double mu) 
 
 void local::DistortedPowerCorrelationFft::transform() {
 #ifdef HAVE_LIBFFTW3
-	// cleanup any previous plan
+	// Clean up any previous plan.
     if(_pimpl->data) FFTW(destroy_plan)(_pimpl->plan);
-    // create a new plan for an in-place transform
+    // Create a new plan for an in-place transform.
     _pimpl->plan = FFTW(plan_dft_3d)(_nx,_ny,_nz,_pimpl->data,_pimpl->data,FFTW_BACKWARD,FFTW_ESTIMATE);
-	// evaluate the power spectrum at each grid point (kx,ky,kz)
+	// Evaluate the power spectrum at each grid point (kx,ky,kz).
 	for(int ix = 0; ix < _nx; ++ix){
 		for(int iy = 0; iy < _ny; ++iy){
 			for(int iz = 0; iz < _nz; ++iz){
+				std::size_t index(iz+_nz*(iy+_ny*ix));
 				double ksq = _kxgrid[ix]*_kxgrid[ix] + _kygrid[iy]*_kygrid[iy] + _kzgrid[iz]*_kzgrid[iz];
             	double k = std::sqrt(ksq);
-            	double mu = _kygrid[iy]/k;
-            	std::size_t index(iz+_nz*(iy+_ny*ix));
-            	_pimpl->data[index][0] = getPower(k,mu);
-            	_pimpl->data[index][1] = 0;
+            	if(k==0) {
+            		_pimpl->data[index][0] = 0;
+            		_pimpl->data[index][1] = 0;
+            	}
+            	if(k>0) {
+            		double mu = _kygrid[iy]/k;
+            		_pimpl->data[index][0] = getPower(k,mu);
+            		_pimpl->data[index][1] = 0;
+            	}
+            	//if(iz==0 && ix==0) std::cout << k << " " << _pimpl->data[index][0] << std::endl;
             }
         }
     }
-    // execute FFT to r space
+    // Execute FFT to r space.
 	FFTW(execute)(_pimpl->plan);
-	// extract the correlation function at grid points (rx,ry,0)
-	for(int iy = 0; iy < _ny; ++iy) {
-        for(int ix = 0; ix < _nx; ++ix) {
+	// Extract the correlation function at grid points (rx,ry,0).
+	//double rvalue = -_spacing;
+	for(int iy = 0; iy < _ny/2+1; ++iy) {
+        for(int ix = 0; ix < _nx/2+1; ++ix) {
         	std::size_t ind(_nz*(iy+_ny*ix));
-        	std::size_t ind2(ix+_nx*iy);
-            _xi[ind2] = (_pimpl->data[ind][0]/_norm);
+        	std::size_t ind2(ix+(_nx/2+1)*iy);
+        	_xi[ind2] = _pimpl->data[ind][0]/_norm;
+        	//if(iy==0) {
+        	//	rvalue += _spacing;
+        	//	std::cout << rvalue << " " << _pimpl->data[ind][0]/_norm << std::endl;
+        	//}
         }
     }
-    // create the bicubic interpolator
-	_bicubicinterpolator = new likely::BiCubicInterpolator(likely::BiCubicInterpolator::DataPlane(_xi),_spacing,_nx,_ny);
+    // Create the bicubic interpolator.
+	_bicubicinterpolator = new likely::BiCubicInterpolator(likely::BiCubicInterpolator::DataPlane(_xi),_spacing,_nx/2+1,_ny/2+1);
 #endif
 }
